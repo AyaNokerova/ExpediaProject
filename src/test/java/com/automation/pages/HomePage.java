2c9c4eaf5691d2d355d8255d676d9a9ce4d1a14d
AyaNokerova <nokerova.a@gmail.com>
2022-08-23 18:43:35 -0400

    commit 1

---
+++ src/test/java/com/automation/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.automation.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+
+    @FindBy(id ="")
+    WebElement userName;
+
+
+}
